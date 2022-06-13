93a644401c86a48d2ca23b3d58fef71176b93b69
Rremesal <raul.remesal@gmail.com>
2022-06-13 11:18:44 +0200

    repository made

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+    <div id="fullpage">
+        <?php include("menu.php");?>
+        <div id="indexContent">
+            <table>
+                <tr>
+                    <th>Voornaam</th>
+                    <th>Tussenvoegsel</th>
+                    <th>Achternaam</th>
+                    <th>Geboortedatum</th>
+                </tr>
+            </table>
+        </div>
+    </div>
+    
+    
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,5 @@
+<div id="menu">
+    <a href="index.php">Overzicht</a>
+    <a href="">Leerling toevoegen</a>
+    <a href="#"></a>
+</div>
