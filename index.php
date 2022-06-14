894dde4e4ba5dc14a3fe64920aa1f239898950f5
Rremesal <raul.remesal@gmail.com>
2022-06-14 18:43:50 +0200

    tabel gemaakt met studenten

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php 
+    include("ziekmeldingendb.php"); 
+    $conn = verbindDB();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,20 +10,34 @@
     <link rel="stylesheet" href="css/style.css"/>
 </head>
 <body>
-    <div id="fullpage">
-        <?php include("menu.php");?>
-        <div id="indexContent">
+        <?php require("menu.php");?>
+        <div class="content">
             <table>
                 <tr>
                     <th>Voornaam</th>
-                    <th>Tussenvoegsel</th>
+                    <th>Tussenvoegsel</h>
                     <th>Achternaam</th>
                     <th>Geboortedatum</th>
+                    <th></th>
                 </tr>
+                <?php 
+                    $selecteerStudentenQuery = "SELECT * FROM studenten";
+                    $stm = $conn->prepare($selecteerStudentenQuery);
+                    $stm->execute();
+                    $studenten = $stm->fetchAll(PDO::FETCH_OBJ);
+                    foreach($studenten as $student) {
+                ?>      <tr>
+                            <td><?= $student->voornaam?></td>
+                            <td><?=$student->tussenvoegsel?></td>
+                            <td><?=$student->achternaam?></td>
+                            <td><?=$student->geboortedatum?></td>
+                            <td>
+                                <a class="statusLink" href="meldingen.php?id=<?=$student->sid?>">Z</a>
+                                <a  class="statusLink" href="beter.php?id=">B</a>
+                            </td>
+                        </tr>
+                <?php } ?>
             </table>
         </div>
-    </div>
-    
-    
 </body>
 </html>
