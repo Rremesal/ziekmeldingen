d2a6945e8b045a40ef2d933084f778299c09f768
Rremesal <raul.remesal@gmail.com>
2022-06-16 11:02:49 +0200

    finished functionality to set student 'sick'

--- meldingen.php
+++ meldingen.php
@@ -2,10 +2,10 @@
 <?php
     include("ziekmeldingendb.php");
     $conn = verbindDB();
-    $query = "SELECT sid FROM studenten WHERE sid=".$_GET['id']."";
-    $stm = $conn->prepare($query);
+    $studentenQuery = "SELECT * FROM studenten";
+    $stm = $conn->prepare($studentenQuery);
     $stm->execute();
-    $studentId = $stm->fetch(PDO::FETCH_OBJ);
+    $studenten = $stm->fetchAll(PDO::FETCH_OBJ);
 ?>
 
 
@@ -20,12 +20,21 @@
     <div class="content" id="meldingenContent">
         <div id="meldingForm">
             <form method="POST">
-                <h2>Ziek Melden</h2>
-                <label for="startdatum">Begindatum:</label>
+                <h2>Ziek melden</h2>
+                <label>Student:</label>
+                <select name="selectStudent">
+                    <?php 
+                        foreach($studenten as $student) {
+                    ?>      <option value="<?=$student->sid?>"><?=$student->voornaam." ".$student->achternaam?></option>
+                    <?php
+                        }
+                    ?>
+                </select>
+                <div><label for="startdatum">Datum afwezigheid</label><div>
                 <div><input type="date" name="txtStartdatum" id="startdatum"/></div>
                 <label for="txtOpmerking">Opmerkingen:</label>
                 <div><textarea name="txtOpmerking" id="txtOpmerking" rows="13" cols="70"></textarea></div>
-                <div><input type="submit" name="btnZiek"/></div>
+                <div><input type="submit" name="btnZiek" id="btnZiek"/></div>
             </form>
         </div>
     </div>
@@ -34,8 +43,9 @@
         if(isset($_POST['btnZiek'])) {
             $toevoegenMeldingQuery = "INSERT INTO ziekmelding (sid,startdatum,opmerking,status) ". 
             "VALUES (:sid,:startdatum,:opmerking,'Ziek')";
+            
             $stm = $conn->prepare($toevoegenMeldingQuery);
-            $stm->bindParam(":sid",$studentId->sid);
+            $stm->bindParam(":sid",$_POST['selectStudent']);
             $stm->bindParam(":startdatum",$_POST['txtStartdatum']);
             $stm->bindParam(":opmerking",$_POST['txtOpmerking']);
             $stm->execute();
