6036042d6e2ce453bb5dec594f2727b0a2502552
Rremesal <raul.remesal@gmail.com>
2022-06-23 11:49:56 +0200

    bindParams toegevoegd

--- ziekmeldingendb.php
+++ ziekmeldingendb.php
@@ -18,7 +18,11 @@ function verbindDB() {
 function toevoegenStudent($voornaam,$tussenvoegsel,$achternaam,$geboortedatum) {
     $conn = verbindDB();
     $toevoegingQuery = "INSERT INTO studenten (voornaam,tussenvoegsel,achternaam,geboortedatum) ". 
-    "VALUES ('$voornaam','$tussenvoegsel','$achternaam','$geboortedatum')";
+    "VALUES (:voornaam,:tussenvoegsel,:achternaam,:geboortedatum)";
     $stm = $conn->prepare($toevoegingQuery);
+    $stm->bindParam(":voornaam",$voornaam);
+    $stm->bindParam(":tussenvoegsel",$tussenvoegsel);
+    $stm->bindParam(":achternaam",$achternaam);
+    $stm->bindParam(":geboortedatum",$geboortedatum);
     return $stm->execute();
 }
